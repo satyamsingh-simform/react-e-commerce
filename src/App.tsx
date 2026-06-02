222ae4422f4aa5bfd26cec6eaee76a22a52630b2
Satyam <satyam.singh@simformsolutions.com>
2026-06-02 15:08:19 +0530

    feat:restaurant card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import FoodOption from "./components/FoodOption";
 import GroceryOption from "./components/GroceryOption";
 import Header from "./components/Header";
+import DineoutOption from "./components/DineoutOption";
 import RestaurantOption from "./components/RestaurantOption";
 
 export default function App(){
@@ -9,6 +10,7 @@ export default function App(){
             <Header/>
             <FoodOption/>
             <GroceryOption/>
+            <DineoutOption/>
             <RestaurantOption/>
         </>
         
--- src/components/RestaurantOption.tsx
+++ src/components/RestaurantOption.tsx
@@ -1,13 +1,57 @@
-import { restaurantsData } from "../utils/restaurantData";
+
+import { useEffect, useState } from "react"
 import RestaurantCard from "./RestaurantCard";
 
+export type RestData={
+    info:{
+        areaName:string,
+        avgRating:number,
+        cloudinaryImageId:string,
+        cuisines:string[],
+        id:string,
+        name:string,
+        sla:{
+            deliverTime:number,
+        },
+    }
+}
+
+type ApiResponse = {
+    data: {
+        cards: {
+            card?: {
+                card?: {
+                    gridElements?: {
+                        infoWithStyle?: {
+                            restaurants: RestData[];
+                        };
+                    };
+                };
+            };
+        }[];
+    };
+};
+
 export default function RestaurantOption(){
+    const [restData,setRestData]=useState<RestData[]>([]);
+    useEffect(()=>{
+        async function fetchData(){
+            const proxyServer = "https://corsproxy.io/?";
+            const swiggyAPI='https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+            const response=await fetch(proxyServer+swiggyAPI);
+            const dataRes:ApiResponse=await response.json();
+            console.log('api-data:',dataRes.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+            setRestData(dataRes.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        }
+        fetchData();
+    },[])
+
     return(
-        <section className="flex flex-col container mx-auto mt-30 mb-30 font-bold text-2xl text-gray-900">
-            <h1 className="mb-10">Discover best restaurants on Dineout</h1>
-            <div className="flex flex-nowrap overflow-x-auto gap-10">
-                {restaurantsData.map((restItem)=><RestaurantCard key={restItem.info.id} restItem={restItem}/>)}
-            </div>
-        </section>
+        <>
+        <h1>hi</h1>
+        {restData.map((item)=>(
+            <RestaurantCard key={item.info.id} item={item}/>
+        ))}
+        </>
     )
 }
